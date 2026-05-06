f347dc68a48d548f8597190d6c20aa7e5c98bc99
maamokun <sachadeco214@gmail.com>
2026-05-06 20:46:01 +0900

    feat: first bad post

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mayu.mikn.dev",
+  site: "https://log.mamomamo.live",
   integrations: [pagefind(), icon(), mdx(), sitemap(), react()],
   vite: {
     plugins: [tailwindcss()],
